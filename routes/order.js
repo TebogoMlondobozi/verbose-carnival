1c94450400646c62b1eb1ac3c6fa268ac1295687
ncedile <ncedile@mcmsolutions.co.uk>
2022-01-30 12:01:19 +0200

    adds a function for updating the order

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const Order = require("../models/order");
 
 const createOrder = require("../middlewares/orders/create-order");
+const UserModel = require("../models/user");
 
 const router = express.Router();
 // create application/json parser
@@ -15,6 +16,40 @@ router.post(
   async function (req, res) {}
 );
 
+router.put(
+  "/update/:orderId/user/:userId",
+  jsonParser,
+  async function (req, res) {
+    UserModel.findById({ _id: req.params.userId })
+      .then((user) => {
+        const { firstname, lastname } = user;
+        Order.findByIdAndUpdate(
+          { _id: req.params.orderId },
+          {
+            items: req.body,
+            note: `${firstname} ${lastname} updated the order on ${Date.now()}`,
+          }
+        )
+          .then(() => {
+            res.send({
+              message: "Successfully updated order",
+              status: true,
+              _id: req.params.orderId,
+            });
+          })
+          .catch((error) => {
+            console.log("1. Failed order update", error);
+            res
+              .status(400)
+              .send({ message: "Failed updating order", status: false });
+          });
+      })
+      .catch((error) => {
+        console.log("Failed finding user..", error);
+        res.status(400).send({ message: "Failed finding user" });
+      });
+  }
+);
 router.get("/:orderId/", function (req, res) {
   Order.findById({ _id: req.params.orderId }, function (err, order) {
     if (order) {
